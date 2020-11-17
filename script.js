1251e2cea930b947f36c89a2b190c43935d24dc4
Andrew Spadaro <andrewspadaro@gmail.com>
2020-11-17 16:19:10 -0500

    made updates to pull wind, temp, and humidity into console

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-$(document).ready(function(){
 
-$("#search-button").on("click", function(){
-var searchValue = $("search-value").val();
+$(document).ready(function () {
 
+    $("#search-button").on("click", function () {
+        var searchValue = $("#search-value").val();
 
-$("#search-value").val("");
-
-searchWeather(searchValue)
-})
+        searchWeather(searchValue)
+    })
 
 
-function searchWeather(searchValue) {
-    $.ajax({
-        type: "GET",
-        url: "api.openweathermap.org/data/2.5/weather?q" + searchValue + "&appid=d612a9005a39f85f6f494b070e6e3182&units=imperial",
-        dataType: "json",
-    }).then(function(data) {
-console.log(data)
-}
+    function searchWeather(searchValue) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=d612a9005a39f85f6f494b070e6e3182&units=imperial",
+            dataType: "json",
+        }).then(function (data) {
+            console.log(data)
+            var temp = data.main.temp;
+            var humidity = data.main.humidity
+            var windSpeed = data.wind.speed
+      
+            getUvIndex(data.coord.lon, data.coord.lat)
 
+            // todo: create html using jquery and inject theese variables
+        })
+    }
 
-)}
+    function getUvIndex(long,lat){
 
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=d612a9005a39f85f6f494b070e6e3182",
+            dataType: "json",
+        }).then(function (data) {
+           console.log(data)
+           // create html to display
+        })
 
+    }
 });
